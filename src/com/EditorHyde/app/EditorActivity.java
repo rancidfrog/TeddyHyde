bbfa4c9a14c786d34a01d361d8b274de8d7ef6e3
Chris Dawson <xrdawson@gmail.com>
2013-03-17 01:26:21 -0400

    Updates for anddown compilation, but there is a crash

--- src/com/EditorHyde/app/EditorActivity.java
+++ src/com/EditorHyde/app/EditorActivity.java
@@ -1,8 +1,13 @@
 package com.EditorHyde.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import  com.commonsware.cwac.anddown.*;
 /**
  * Created with IntelliJ IDEA.
  * User: xrdawson
@@ -10,12 +15,39 @@ import android.os.Bundle;
  * Time: 1:24 AM
  * To change this template use File | Settings | File Templates.
  */
-public class EditorActivity extends Activity {
+public class EditorActivity extends Activity implements View.OnClickListener {
+
+   AndDown ad;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editor);
 
+        ad = new AndDown();
+
+        ImageButton renderButton = (ImageButton) findViewById(R.id.imageButton);
+        renderButton.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if (id == R.id.imageButton) {
+            renderToWebView();
+        }
+    }
+
+    private void renderToWebView() {
+        TextView tv = (TextView) findViewById(R.id.editText1);
+        String rendered = ad.markdownToHtml( tv.toString() );
+        Intent i = new Intent(this, RenderedActivity.class);
+        i.putExtra( "rendered", rendered );
+        startActivity(i);
+    }
+
+
 }
