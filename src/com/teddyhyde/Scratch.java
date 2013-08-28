9123c34bc50c2e5f78ae19ebf9a57cd2b1fefa72
Chris Dawson <xrdawson@gmail.com>
2013-08-27 21:57:44 -0700

    Added scratchpad into proper tree

---
+++ src/com/teddyhyde/Scratch.java
@@ -0,0 +1,42 @@
+package com.teddyhyde;
+
+/**
+ * Created by xrdawson on 8/19/13.
+ */
+public class Scratch {
+
+    private long id;
+    private String comment;
+    private String scratch;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getScratch() {
+        return scratch;
+    }
+
+    public void setScratch(String scratch) {
+        this.scratch = scratch;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
+
