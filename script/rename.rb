4650647f4820ad12a6ffc87370f373984aa2574f
Chris Dawson <xrdawson@gmail.com>
2013-09-26 11:11:14 -0700

    First version of rewrite package script

--- script/rename.rb
+++ script/rename.rb
@@ -6,16 +6,21 @@ to = ARGV.shift
 unless from and to
   puts "Need both from package and to package"
 else
-  from.split( "." ).each do |d|
-    Dir.chdir d
-  end
+  to = "src/" + to
+  from = "src/" + from
+  from_dir = from.gsub ".", "/"
+  to_dir = to.gsub ".", "/"
 
-  # Do stuff within this directory
+  puts "From: #{from_dir}, To: #{to_dir}"
   
+  files = Dir.entries( from_dir ).reject { |d| d =~ /^\.\.?$/ }
 
-  # Restore the directory
-  from.split( "." ).each do |d|
-    Dir.chdir ".."
+  files.each do |f|
+    contents = File.read( File.join( from_dir, f ), "r" )
+    updated = contents.gsub( from, to )
+    File.open( File.join( to_dir, f ), "w+" ) do |o|
+      o.write contents
+    end
   end
   
 end
