8de19bc2cf492d10f0d98e17ba4d9b57b997b539
Chris Dawson <xrdawson@gmail.com>
2013-10-22 12:44:01 -0700

    Trying to fix test issues

--- src/test/java/com/EditorHyde/app/MainActivityTest.java
+++ src/test/java/com/EditorHyde/app/MainActivityTest.java
@@ -4,11 +4,26 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 
+import java.lang.Exception;
+import java.lang.Override;
+
 import static org.fest.assertions.api.ANDROID.assertThat;
+import static org.junit.Assert.*;
+//import org.junit.Assert;
 
 @RunWith(RobolectricGradleTestRunner.class)
 public class MainActivityTest {
+
+
+    // @Before public void setUp() throws Exception {
+    //     Runtime rt = Runtime.getRuntime();
+    //     Process proc = rt.exec( "curl --data '{\"scopes\":[\"gist\"],\"note\":\"Demo\"}\' --user \"burning@burningon.com\" -i https://api.github.com/authorizations" );
+    // }
+
     @Test public void heightRespectsWidth() {
+        assertTrue( "This is true", true );
+        assertTrue( "Nope, this is false", false );
+        
         // SquaredImageView view = new SquaredImageView(Robolectric.application);
 
         // int width = MeasureSpec.makeMeasureSpec(100, MeasureSpec.EXACTLY);
