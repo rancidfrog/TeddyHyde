402a2806963a43023ab8dfbfbaa9fc4241091392
Chris Dawson <xrdawson@gmail.com>
2013-10-30 13:33:57 -0700

    Got working calabash scripts up to new markdown file

--- calabash/features/my_first.feature
+++ calabash/features/my_first.feature
@@ -2,8 +2,11 @@ Feature: Add file to repository
   Scenario: As a logged in user I can add a file
     Given I start the login process
     And I acknowledge the 2 factor bug
-    Then I wait 10 seconds
+    Then I wait for 10 seconds
     And I login using GitHub oAuth login
     And I access the first repository
-    And I press "New Post"
-	    
+    Then I wait for 10 seconds
+    And I select "New Post" 
+    And I choose a filename
+    And I press "Ok"
+    Then I should see the text containing "markdown"
--- calabash/features/step_definitions/login.rb
+++ calabash/features/step_definitions/login.rb
@@ -5,29 +5,40 @@ end
 
 And /I acknowledge the 2 factor bug/ do
       tv = query( "textview", "text" )
-      if tv =~ /2\-factor/ 
-      touch "button"
+      if tv[0] =~ /2\-factor authentication/ 
+      touch "button text:'Ok'"
       end   
 end   
 
 
-Then(/^I wait (\d+) seconds$/) do |arg1|
-  sleep 10
+And /I select "New Post"/ do
+    touch "ActionMenuItemView id:'action_add_new_post'"
 end
 
-And /I access the first repository/ do
-      tv = query "textview"
-      touch tv
+Then(/^I should see the text containing "(.*?)"$/) do |arg1|
+   if arg1 =~ /markdown/i
+   puts "Got text!"
+   end
 end
 
-And /I press "New Post"/ do
-      np = query "TextView text:'New post'"
-      touch np
+And /I choose a filename/ do
+    query "edittext", :setText => "My first file"
 end
 
 And /I login using GitHub oAuth login/ do
       set_text "webView css:input[name=login]", "burning@burningon.com"
       set_text "webView css:input[name=password]", "L0udCaf3"
-      performAction "scroll_to", "css", "a.oauth-deny"
-      touch  "webView css:'button[name=authorize]'"
+      touch "webView input:'button[type=submit]'"
+      sleep 10
+      text = query( "webView css:'.bubble-content'", "textContent" )[0]
+      if text =~ /Teddy Hyde Android wants/
+            performAction "scroll_to", "css", "a.oauth-deny"
+      	    touch "webView css:'button[name=authorize]'"
+      end	    
+      sleep 10
 end    
+
+And /I access the first repository/ do
+touch( "listview textview" )
+end 
+
