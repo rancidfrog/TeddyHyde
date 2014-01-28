dd0cdf82bcf2660d1fe505667a730ae7711756e9
Chris Dawson <xrdawson@gmail.com>
2014-01-27 20:51:47 -0800

    Deleted files

--- src/main/assets/js/teddyhyde.js
+++
@@ -1,5 +0,0 @@
-console.log( "Inside the asciidoc rendering..." );
-var html  = Opal.Asciidoctor.$render( asciidocMarkup );
-console.log( "Rendered..." );
-document.write( html );
-console.log( "Inserted" );
--- src/main/java/com/EditorHyde/app/PrettyFilenameDialog.java
+++
@@ -1,7 +0,0 @@
-package com.EditorHyde.app;
-
-/**
- * Created by xrdawson on 12/5/13.
- */
-public class PrettyFilenameDialog {
-}
