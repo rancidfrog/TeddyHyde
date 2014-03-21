21b41fba930eeb48ec677a328abd62f8343133fe
Chris Dawson <xrdawson@gmail.com>
2014-03-21 16:43:53 -0700

    Fixes to build under gradle 1.11 and new Android Studio

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,7 @@
+echo "Make sure to run under gradle 1.11 (something like: PATH=~/bin/gradle-1.11/bin:\$PATH ./script/build.sh password)"
+
 if [[ "$1" != "" ]]; then
-    INQRY_PASSWORD=$1 STORE_FILE=~/android/inqry_keystore gradle assembleRelease $@
+    INQRY_PASSWORD=$1 STORE_FILE=~/android/inqry_keystore gradle assembleRelease 
 else
     echo "build.sh <password>"
 fi
